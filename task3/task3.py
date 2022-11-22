551e59ab9f3216adf202c9ca5037f56a9a6a9c0b
hivaze <hivaze.me@gmail.com>
2022-11-22 04:18:36 +0300

    Initial commit

---
+++ task3/task3.py
@@ -0,0 +1,46 @@
+# Гр. БПМ-19-2, Антонов Илья
+
+from io import StringIO
+import csv
+
+def task(csvString):
+    f = StringIO(csvString)
+    reader = csv.reader(f, delimiter=',')
+    graph = []
+    for row in reader:
+        graph.append(row)
+    for x in graph:
+        for y in x:
+            y = int(y)
+    # Отношение 1 - прямое управление
+    arr1 = []
+    for x in graph:
+        if x[0] not in arr1:
+            arr1.append(str(x[0]))
+    # Отношение 2 - прямое подчинение
+    arr2 = []
+    for x in graph:
+        if x[1] not in arr2:
+            arr2.append(str(x[1]))
+    # Отношение 3 - косвенное управление
+    f = graph
+    g = graph
+    arr3 = []
+    for i in range(len(f)):
+        for j in range(len(g)):
+            if i != j and f[i][0] not in arr3 and f[i][1] == g[j][0]:
+                arr3.append(str(f[i][0]))
+    # Отношение 4 - косвенное подчинение
+    arr4 = []
+    for i in range(len(f)):
+        for j in range(len(g)):
+            if i != j and f[i][1] not in arr4 and f[i][0] == g[j][1]:
+                arr4.append(str(f[i][1]))
+    # Отношение 5 - соподчинение
+    arr5 = []
+    for i in range(len(f)):
+        for j in range(len(g)):
+            if i != j and f[i][1] not in arr5 and f[i][0] == g[j][0]:
+                arr5.append(str(f[i][1]))
+    # Результаты
+    return [arr1, arr2, arr3, arr4, arr5]
